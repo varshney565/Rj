a9697ee7f057e69ee9e6eeb7dcfb20e703d86fda
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2022-10-15 20:32:53 +0530

    Lecture2Completed

---
+++ ArrayAndString/Lecture2/3.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define pi pair<int,int>
+#define ff first
+#define ss second
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+inline void read(int &n,vector<int> &v){for(int i = 0 ; i < n ; i++) cin>>v[i];}
+inline void print(int &n,vector<int> &v){for(int i = 0 ; i < n ; i++) cout<<v[i]<<" ";cout<<"\n";}
+inline void read(int &n,int &m,vector<vector<int>> &v){for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>v[i][j];}}}
+inline void print(int &n,int &m,vector<vector<int>> &v){for(int i=0 ;i<n;i++){for(int j=0;j<m;j++){cout<<v[i][j]<<" ";}cout<<"\n";}}
+
+int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    int i = 0;
+    int j = 0;
+    map<int,int> cnt;
+    int ans = 0;
+    while(i < n){
+        cnt[s[i]]++;
+        while(cnt[s[i]] > 1){
+            cnt[s[j]]--;
+            j++;
+        }
+        ans = max(ans,i-j+1);
+        i++;
+    }
+    return ans;
+}
+
+void solve(){
+   string s;
+   cin>>s;
+   cout<<lengthOfLongestSubstring(s)<<"\n";
+}
+
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("../../IO/input.txt","r",stdin);
+        freopen("../../IO/output.txt","w",stdout);
+        freopen("../../IO/error.txt", "w", stderr);
+    #endif
+
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    return 0;
+}
+
