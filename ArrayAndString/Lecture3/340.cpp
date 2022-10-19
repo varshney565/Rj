26efc5fb420aaa111c0f3da856809e38dc88fa15
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2022-10-19 10:08:11 +0530

    Lecture3Completed

---
+++ ArrayAndString/Lecture3/340.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#pragma GCC optimise("Ofast")
+#pragma GCC target("avx","avx2","fma")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+using namespace std;
+#define int long long
+#define all(x) x.begin(),x.end()
+#define pi pair<int,int>
+#define ff first
+#define ss second
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(multimap <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+inline void read(int &n,vector<int> &v){for(int i = 0 ; i < n ; i++) cin>>v[i];}
+inline void print(int &n,vector<int> &v){for(int i = 0 ; i < n ; i++) cout<<v[i]<<" ";cout<<"\n";}
+inline void read(int &n,int &m,vector<vector<int>> &v){for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>v[i][j];}}}
+inline void print(int &n,int &m,vector<vector<int>> &v){for(int i=0 ;i<n;i++){for(int j=0;j<m;j++){cout<<v[i][j]<<" ";}cout<<"\n";}}
+
+int atmostK(string s,int k){
+    int n = s.size();
+    map<int,int> cnt;
+    int en = 0;
+    int st = 0;
+    int ans = 0;
+    while(en < n){
+        cnt[s[en]]++;
+        while(cnt.size() > k){
+            cnt[s[st]]--;
+            if(cnt[s[st]] == 0)
+                cnt.erase(s[st]);
+            st++;
+        }
+        ans = max(ans,en-st+1);
+        en++;
+    }
+    return ans;
+}
+
+void solve(){
+   
+}
+
+signed main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("../../IO/input.txt","r",stdin);
+        freopen("../../IO/output.txt","w",stdout);
+        freopen("../../IO/error.txt", "w", stderr);
+    #endif
+
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<"\n";
+    return 0;
+}
+
