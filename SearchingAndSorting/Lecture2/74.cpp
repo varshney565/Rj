1b88c3e6f18a030f9f6409d1beff6811457dca07
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2022-11-04 18:37:38 +0530

    Lecture2Done

---
+++ SearchingAndSorting/Lecture2/74.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(),m = matrix[0].size(),s = 0,e = n*m-1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            if(matrix[mid/m][mid%m] < target){
+                s = mid+1;
+            }else if(matrix[mid/m][mid%m] > target){
+                e = mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
