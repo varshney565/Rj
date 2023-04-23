e6fa45c25678e88acdeedf65b7dce81fe4bdd9ba
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-04-23 14:45:01 +0530

    ArticulationPoint&&&&Bridges

--- Implementation/Articulation_point.cpp
+++ Implementation/Articulation_point.cpp
@@ -2,10 +2,28 @@ void ap(vector<vector<int>> &graph){
 	int n = graph.size();
 	vector<int> low(n,-1),in(n,-1);
 	vector<bool> visited(n,false),ans(n,false);
-	function<void(int,int)> dfs = [&](int src,int par){
-		
+	int time = 0;
+	function<void(int,int)> dfs = [&](int u,int p){
+		visited[u] = true;
+		in[u] = low[u] = time++;
+		int children = 0;
+		for(auto child : graph[u]){
+			if(child == p) continue;
+			if(visited[child]){
+				low[u] = min(low[u],in[child]);
+			}else{
+				dfs(child,u);
+				children++;
+				low[u] = min(low[u],low[child]);
+				if(low[child] >= in[u]){
+					ans[u] = true;
+				}
+			}
+		}
+		if(p == -1 && children > 1) ans[u] = true;
 	};
 	for(int i = 0 ; i < n ; i++){
 		if(!visited[i]) dfs(i,-1);
 	}
+	//print all the articulation points
 }
